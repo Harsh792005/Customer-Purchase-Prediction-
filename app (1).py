c754df3195ae44202295fda63d37e4e71fa17b2c
harsh kumar shrivastav <harshshrivastav792005@gmail.com>
2026-06-25 22:45:53 +0530

    Add new routes for about, contact, and predict page

--- app (1).py
+++ app (1).py
@@ -12,6 +12,18 @@ model = joblib.load("model.pkl")
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/predictpage")
+def predictpage():
+    return render_template("index.html")
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
